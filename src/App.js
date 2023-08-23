15f5f1cf10a52bf74a17080010c647584f6843f5
Ellie Kang <ellie.kang@mail,utoronto.ca>
2023-08-23 15:32:49 -0400

    upload form visually finalized

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Header from './components/Header';
+// import Header from './components/Header';
 import Form from './components/Form';
 
 function App() {
   return (
     <div>
       <div className='main-comp'>
-        <Header />
+        {/* <Header /> */}
         <Form />
       </div>
     </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,59 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import './Form.css';
 
 export default function Form() {
+
+    const soccerFamousCountries = [
+        'Argentina',
+        'Belgium',
+        'Brazil',
+        'Cameroon',
+        'Chile',
+        'Colombia',
+        'Croatia',
+        'England',
+        'France',
+        'Germany',
+        'Ghana',
+        'Italy',
+        'Ivory Coast',
+        'Korea, Republic of',
+        'Mexico',
+        'Netherlands',
+        'Nigeria',
+        'Portugal',
+        'Russia',
+        'Spain',
+        'Uruguay'].sort();
+    const uniformPrice = [
+        '$0 ~ $50',
+        '$50 ~ $100',
+        '$100 ~ $150',
+        '$150 ~ $200',
+        '$200 ~ $300',
+        '$300 ~ $400',
+        '$400 ~ $500',
+        '$500 ~ $700',
+        '$700 ~ $900',
+        '$900 ~ $1000',
+        '$1000 +'
+    ]
+    const uniformSize = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
     
     const [formData, setFormData] = useState({
-        imageString: '',
+        // imageString temporarily removed
+        // imageString: '',
         price: '',
-        teamOrcountry: '',
+        team:'',
+        country: '',
         size: '',
         productDescription: '',
         playerName: '',
         uniformYear: '',
         sellLocation: '',
         sellerLocation: '',
-        sellerName: ''
+        sellerName: '',
     });
-
-    // const options = useMemo(() => countryList().getData(), []);
 
     const handleChange = (e) => {
     const { name, value } = e.target;
@@ -28,54 +65,90 @@ export default function Form() {
     console.log(formData);
     }
 
+
    return (
         <div className="form-container">
-            <h1 className="form-logo">🎽</h1>
-            <h1 className="form-title">Upload your uniform and make it valuable!</h1>
-            <form onSubmit={handleSubmit}>
-                <label className="eachItem">
-                    Image String:
-                    <input type="text" name="imageString" value={formData.imageString} onChange={handleChange} />
+            {/* <h1 className="form-logo">🎽</h1> */}
+            <h1 className="form-title">Upload your uniform</h1>
+            <h1 className="form-subtitle">Tell us more about your uniform so that yours can be more valuable!</h1>
+            
+            <form className="form-container2">
+                
+                <label className="label-text">
+                    Uniform Image
                 </label>
-                <label className="eachItem">
-                    Price:
-                    <input type="text" name="price" value={formData.price} onChange={handleChange} />
-                </label>
-                <label className="eachItem">
-                    Team/Country:
-                    <input type="text" name="teamOrcountry" value={formData.teamOrcountry} onChange={handleChange} />
 
+                <div className='image-upload'>
+                    <img className='uniform-img' alt='' src={require('./uniform.png')} />
+                    <button className='upload-btn'>Upload Image</button>
+                </div>
 
+                <label className="label-text">
+                    Price
                 </label>
-                <label className="eachItem">
-                    Size:
-                    <input type="text" name="size" value={formData.size} onChange={handleChange} />
+                <select name="price" value={formData.price} onChange={handleChange}>
+                    {uniformPrice.map((c, index) => <option key={index} value={c}>{c}</option>)}
+                </select>
+
+            
+                <label className="label-text">
+                    Country
                 </label>
-                <label className="eachItem">
-                    Product Description:
-                    <input type="text" name="productDescription" value={formData.productDescription} onChange={handleChange} />
+                <select name="country" value={formData.country} onChange={handleChange}>
+                    {soccerFamousCountries.map((c, index) => <option key={index} value={c}>{c}</option>)}
+                </select>
+
+                <label className="label-text">
+                    Team
                 </label>
-                <label className="eachItem">
-                    Player Name:
-                    <input type="text" name="playerName" value={formData.playerName} onChange={handleChange} />
+
+                <input type="text" name="team" value={formData.team} onChange={handleChange} />
+                
+                <label className="label-text">
+                    Size
                 </label>
-                <label className="eachItem">
-                    Uniform Year:
-                    <input type="text" name="uniformYear" value={formData.uniformYear} onChange={handleChange} />
+
+                <select name="size" value={formData.size} onChange={handleChange}>
+                    {uniformSize.map((c, index) => <option key={index} value={c}>{c}</option>)}
+                </select>
+
+                <label className="label-text">
+                    Product Description
                 </label>
-                <label className="eachItem">
-                    Sell Location:
-                    <input type="text" name="sellLocation" value={formData.sellLocation} onChange={handleChange} />
+
+                <input type="text" name="productDescription" value={formData.productDescription} onChange={handleChange} />
+
+                <label className="label-text">
+                    Player Name
                 </label>
-                <label className="eachItem">
-                    Seller Location:
-                    <input type="text" name="sellerLocation" value={formData.sellerLocation} onChange={handleChange} />
+
+                <input type="text" name="playerName" value={formData.playerName} onChange={handleChange} />
+
+                <label className="label-text">
+                    Uniform Year
                 </label>
-                <label className="eachItem">
-                    Seller Name:
-                    <input type="text" name="sellerName" value={formData.sellerName} onChange={handleChange} />
+
+                <input type="text" name="uniformYear" value={formData.uniformYear} onChange={handleChange} />
+
+                <label className="label-text">
+                    Sell Location
                 </label>
-                <button type="submit">Submit</button>
+
+                <input type="text" name="sellLocation" value={formData.sellLocation} onChange={handleChange} />
+
+                <label className="label-text">
+                    Your Location
+                </label>
+
+                <input type="text" name="sellerLocation" value={formData.sellerLocation} onChange={handleChange} />
+
+                <label className="label-text">
+                    Your Name
+                </label>
+
+                <input type="text" name="sellerName" value={formData.sellerName} onChange={handleChange} />
+
+                <button onClick={handleSubmit} className="submit-btn">Submit</button>
             </form>
         </div>
    )
